4c2d53a8c44663bdc51006ef6be91fe682f16fd8
Nicolas Boullon <boullon.nicolas@gmail.com>
2024-03-08 10:16:40 -0300

    Creacion de Ajax

---
+++ frontend/script.js/ajax.js
@@ -0,0 +1,45 @@
+
+
+
+const form = document.getElementById("form-fetch");
+
+form.addEventListener("submit",function(e){
+    e.preventDefault();
+    const formData = new FormData(this);
+    crearUsuario_ajax(formData);
+
+})
+
+function crearUsuario_ajax(nuevoUsuario){
+
+    const xhr =  new XMLHttpRequest(); // el xhr puede variar, podria ser "peticion" por ej
+
+    //setear el evento ready state change. Tiene cinco valores. si vale 4, significa q
+    //recibimos la respuesta del sv
+    xhr.onreadystatechange = ()=>
+    {
+        if(xhr.readyState == 4)
+        {
+            if(xhr.status >= 200 && xhr.status < 300)
+            {
+                const data = JSON.parse(xhr.responseText);
+                console.log(data);
+            }
+            else{
+                console.error(`Error ${xhr.status}: ${xhr.statusText}`);
+            }
+        }
+    }
+
+    xhr.open("POST", URL, true);
+    
+    //seteo la cabecera
+    xhr.setRequestHeader("Content-Type","application/json;charset=UTF-8");
+    
+    //enviar la peticion
+    try { // por si falla otra cosa
+        xhr.send(JSON.stringify(nuevoUsuario));
+    } catch (err) {
+        console.error(err)
+    }
+}
